80a62c61e3c94ec019f91938940a330fb4c4f04b
eunJeong <poilchxn@imform.co.kr>
2020-11-10 18:48:37 +0900

    study

--- programmers/challenge3/index.js
+++ programmers/challenge3/index.js
@@ -15,15 +15,3 @@ process.stdin.on('data', data => {
     }
     console.log(txt)
 });
-
-/* 
-process.stdin.setEncoding('utf8');
-process.stdin.on('data', (data) => {
-  const n = data.split(' ');
-  const a = Number(n[0]),
-    b = Number(n[1]);
-  const row = `${'*'.repeat(a)}\n`;
-  const col = row.repeat(b);
-  console.log(col);
-});
-*/
