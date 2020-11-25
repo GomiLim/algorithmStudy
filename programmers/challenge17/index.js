ab6e955e58be41f6ff3f8e5c484fa888c7a7cb87
GomiLim <poil_chxn@naver.com>
2020-11-26 00:16:13 +0900

    update

--- programmers/challenge17/index.js
+++ programmers/challenge17/index.js
@@ -6,51 +6,48 @@
 예를 들어 s가 pPoooyY면 true를 return하고 Pyy라면 false를 return합니다.
 */
 
-//초간단
+//다른사람의 풀이
 function solution(s) {
-  console.log(s.toUpperCase().split('P').length)
-  console.log(s.toUpperCase().split('Y'))
   return s.toUpperCase().split("P").length === s.toUpperCase().split("Y").length;
-  // return s.split('P').length === s.match(/Y/gi).length ? true : false;
   //충격적이다 정말
 }
 //초간단
-// function solution(s) {
-//   return s.match(/P/gi).length === s.match(/Y/gi).length ? true : false;
-//   //근데 런타임 에러걸림
-// }
+function solutionMatch(s) {
+  return s.match(/P/gi).length == s.match(/Y/gi).length ? true : false;
+//근데 런타임 에러걸림
+//true false 를 안햐ㅐ줘도 돠ㅣㄴ느군아아아아
+//numPY("Izc") 이런 식으로 p나 y가 없는 경우에는 오류가 납니다....
+//해결책 (s.match(/p/ig)||[]).length == (s.match(/y/ig)||[]).length
+}
 
 //2번째
-// function solution(s) {
-//   var P=[];
-//   var Y=[];
+function solutionSecond(s) {
+  var P=[];
+  var Y=[];
 
-//   Array.from(s.toUpperCase()).filter((ele)=> {
-//     (ele.indexOf('P') !== -1) && P.push(ele);
-//     (ele.indexOf('Y') !== -1) && Y.push(ele);
-//   })
-//   return P.length ===  Y.length ? true : false
-// }
+  Array.from(s.toUpperCase()).filter((ele)=> {
+    (ele.indexOf('P') !== -1) && P.push(ele);
+    (ele.indexOf('Y') !== -1) && Y.push(ele);
+  })
+  return P.length ===  Y.length ? true : false
+}
 
 //초기버전
-// function solution(s) {
-//   var P='';
-//   var Y='';
+function solutionFirst(s) {
+  var P='';
+  var Y='';
 
-//   Array.from(s.toUpperCase()).filter((ele)=> {
-//     if(ele.indexOf('P') !== -1){
-//       P += ele;
-//     }
-//     if(ele.indexOf('Y') !== -1){
-//       Y += ele;
-//     }
-//   })
+  Array.from(s.toUpperCase()).filter((ele)=> {
+    if(ele.indexOf('P') !== -1){
+      P += ele;
+    }
+    if(ele.indexOf('Y') !== -1){
+      Y += ele;
+    }
+  })
 
- 
-
-//   return P.length ===  Y.length ? true : false
-// }
-
+  return P.length ===  Y.length ? true : false
+}
 
 console.log(solution('pPoooyY'))
 //true
