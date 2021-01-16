ed4c31bc06ea8496c0509a350d249459c0ac3e19
GomiLim <poil_chxn@naver.com>
2021-01-17 03:04:19 +0900

    두 개 뽑아 더하기

--- programmers/challenge25/index.js
+++ programmers/challenge25/index.js
@@ -11,73 +11,34 @@ numbers	result
 [2,1,3,4,1]	[2,3,4,5,6,7]
 [5,0,2,7]	[2,5,7,9,12]
 */
-
-function solution(board, moves) {
-  const result_list = [];
-  let pop_item_count = 0;
-
-  for (let i = 0; i < moves.length; i++) {
-    let real_move_idx = moves[i] - 1;
-    for (let j = 0; j < board.length; j++) {
-      if (board[j][real_move_idx] != 0) {
-        let top_item = board[j][real_move_idx];
-        // console.log(`found item: ${top_item} / index: ${real_move_idx}`);
-
-        board[j][real_move_idx] = 0;  // 아이템 뽑았기에 0으로 아이템 없애준다
-        if (result_list.length > 0) {
-          // 결과 바구니에 인형이 존재할 때
-          if (result_list[result_list.length - 1] == top_item) {
-            // 결과 바구니 맨 위 아이템이 방금 뽑은 아이템과 동일할 때
-            pop_item_count++;
-            result_list.pop();
-          } else {
-            //  동일하지 않을 때 - 아이템 추가
-            result_list.push(top_item);
-          }
-        } else {
-          // 바구니가 비었을 때
-          result_list.push(top_item);
-        }
-
-        // if (result_list.length > 0 && result_list[result_list.length - 1] == top_item) {
-        //   // console.log(`last item: ${result_list[result_list.length - 1]}`)
-        //   pop_item_count++;
-        //   result_list.pop();
-        // } else {
-        //   // console.log(`new item add`);
-        //   result_list.push(top_item);
-        // }
-        break;
+function solution(numbers) {
+  let list = new Set;
+  for(let i = 0; i < numbers.length; i ++) {
+    for(let j = 0; j < numbers.length; j ++) {
+      if (i !== j) {
+        list.add(numbers[i]+numbers[j])
       }
     }
-    // console.log(result_list)
   }
-  
-  // for (let i = 0; i < moves.length; i++) {
-  //   console.log(board[i][moves[i]-1]);
-  // }
 
-  return pop_item_count * 2;    // 2개씩 터지므로 2배
+  return [...list].sort((a,b) => a-b);
 }
 
+function solutionImdexOf(numbers) {
+  var answer = [];
+  //들어온 배열의 요소를 확인한다.
+  //배열의 두개 요소를 뽑아 덧셈한 모든 경우의 수를 구한다.
+  //중복된 값은 제거한다.
+  //배열값을 리턴한다.
+  for(let i=0; i<numbers.length-1; i++){
+      for(let j=i+1; j<numbers.length; j++){
+         if(answer.indexOf(numbers[i]+numbers[j])===-1){
+             answer.push(numbers[i]+numbers[j]) 
+         } 
+      }
+  }
 
-/**
- * 자료구조
- * 
- * 1. 링크드 리스트
- * 2. 큐
- * 3. 스택
- * 4. 해쉬
- * 5. 맵
- * 6. 집합 (set)
- * 7. 사전 (dictionary - object)
- * 8. 배열
- * 
- 큐 FIFO First in First out
- 스택 FILO First in Last out
- */
+  return answer.sort((a,b)=>a-b);
+}
 
-
-
-
-console.log(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4]));
+console.log(solution([2,1,3,4,1]));
