e8024d534a2d374def4d06223efdbf4a5c5127e5
GomiLim <poil_chxn@naver.com>
2021-01-19 21:58:55 +0900

    자연수 뒤집어 배열로 만들기

---
+++ programmers/challenge30/index.js
@@ -0,0 +1,24 @@
+/*
+자연수 뒤집어 배열로 만들기
+문제 설명
+자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+
+제한 조건
+n은 10,000,000,000이하인 자연수입니다.
+입출력 예
+n	return
+12345	[5,4,3,2,1]
+*/
+
+function solution(n) {
+  let result = [...n.toString()].reverse().map(item => Number(item));
+  return result;
+}
+
+
+
+function solutionParseInt(n) {
+  console.log(n+'')
+    return (n + '').split('').reverse().map(n => parseInt(n));
+}
+console.log(solutionParseInt(12345));
