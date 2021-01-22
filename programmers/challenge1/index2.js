9be1b5835a9c8d1b1217e95ec5b4262114529f6d
GomiLim <poil_chxn@naver.com>
2021-01-22 23:21:45 +0900

    challenge1 update

--- programmers/challenge1/index2.js
+++ programmers/challenge1/index2.js
@@ -1,9 +1,11 @@
+/*
+    단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.<br />
+    재한사항<br />
+    s는 길이가 1 이상, 100이하인 스트링입니다.<br />
+*/ 
+
 function solution(s) {
-    var answer = '';
-    // const mid = Math.floor(s.length/2);
-    // parseInt  (뒤에 소수점도 짤리던데, 타입변환 할때만 주로 사용 ? 소수점 뒤에 짜를때 Math ? toLocalString() )
-    answer = ( s.length % 2 ) ? s[Math.floor(s.length /2)] : s[Math.floor([s.length/2])-1] +s[Math.floor([s.length/2])];
-    return answer;
+return [...s].length % 2 ? [...s][length/2] : [...s][length/2][length/2+1];
 }
+
 console.log(solution('일이삼사오육칠팔구'));
-selectElement('.answer').innerHTML = solution('일이삼사오육칠팔구십');
