d6fec9c3eb437c23c563d84ef8d5d99722ead25f
GomiLim <poil_chxn@naver.com>
2021-01-23 23:08:13 +0900

    가운데 글자 가져오기

---
+++ programmers/challenge1/again.js
@@ -0,0 +1,14 @@
+/*
+    단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.<br />
+    재한사항<br />
+    s는 길이가 1 이상, 100이하인 스트링입니다.<br />
+*/ 
+
+function solution(s) {
+    console.log(s[Math.floor(s.length/2)])
+    let center = Math.floor(s.length/2);
+    console.log(center)
+return s.length % 2 ? s[center] : s[center-1]+s[center];
+}
+
+console.log(solution('일이삼사오육칠팔구십'));
