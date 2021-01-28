6cea99725ab481f2d55563cc846f6b90bebaff97
eunJeong <poilchxn@imform.co.kr>
2021-01-28 23:58:23 +0900

    나누어 떨어지는 숫자 배열

---
+++ programmers/challenge12/again.js
@@ -0,0 +1,33 @@
+/*
+array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수,
+solution을 작성해주세요.
+divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
+
+제한사항
+arr은 자연수를 담은 배열입니다.
+정수 i, j에 대해 i ≠ j 이면 arr[i] ≠ arr[j] 입니다.
+divisor는 자연수입니다.
+array는 길이 1 이상인 배열입니다.
+
+arr	divisor	return
+[5, 9, 7, 10]	5	[5, 10]
+[2, 36, 1, 3]	1	[1, 2, 3, 36]
+[3,2,6]	10	[-1]
+
+*/
+
+function solution(arr, divisor) {
+  let _arr = [];
+  const newArray = [...arr].filter((val) => {
+    if (val % divisor === 0) {
+      return val;
+    } else {
+      _arr.push(val);
+    }
+  });
+
+  return _arr.length === arr.length ? [-1] : newArray.sort();
+}
+
+console.log(solution([2, 36, 1, 3], 1));
+//[5, 10]
